f1eb73df3300c27c131c96a95082640c83465d69
Mind-Glitch <63287416+Mind-Glitch@users.noreply.github.com>
2022-09-30 13:55:47 +0000

    Update UnitTest1.cs

--- Crazy_Panda/Crazy_Panda_Test_Task/UnitTest/UnitTest1.cs
+++ Crazy_Panda/Crazy_Panda_Test_Task/UnitTest/UnitTest1.cs
@@ -6,13 +6,10 @@ namespace UnitTest
         [Fact]
         public void MakeTest()
         {
-            int start;
-            int end;
-
             //test 1
             List<uint> list1 = new() { 5, 3, 9, 11 };
             
-            Function.FindElementsForSum(list1, 8, out start, out end);
+            Function.FindElementsForSum(list1, 8, out int start, out int end);
             Assert.True(start == 0 && end == 2);
 
             Function.FindElementsForSum(list1, 20, out start, out end);
